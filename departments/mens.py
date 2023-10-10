6057fc32919453fe2da6c220d480b08e1ba93cea
Nathan Chevalier <cerberusgfx@gmail.com>
2023-10-10 10:49:11 -0500

    Sizes added to departments

---
+++ departments/mens.py
@@ -0,0 +1,3 @@
+class Mens:
+    def __init__(self):
+        self.sizes = ['M-XS', 'M-S', 'M-M', "M-LG", "M-XL"]
---
+++ departments/youths.py
@@ -0,0 +1,3 @@
+class Youths:
+    def __init__(self):
+        self.sizes = ['Y-XS', 'Y-S', 'Y-M', "Y-LG", "Y-XL"]
